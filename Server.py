342fbb24e34443e5a9224fea05e9cfeb5c8a6156
crh5914 <249683003@qq.com>
2017-09-20 21:50:03 +0800

    add mento page

--- Server.py
+++ Server.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+@app.route('/mento')
+def mento():
+	return render_template('mento.html')    
 if __name__ == '__main__':
     app.run(debug=True)
